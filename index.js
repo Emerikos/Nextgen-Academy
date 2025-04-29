b97c0b98f9fe851e160fe4f507dd9bb7a9e74619
unknown <nwachukwugodwin7@gmail.com>
2025-04-29 07:11:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// // three ways of declaring variables
+// console.log('variables');
+
+// //full_name is a camel type naming convention
+// let full_name='Godwin Nwachukwu'
+// console.log(full_name)
+// console.log(`my_name_is:${full_name}`);
+
+// let a=('book');
+// console.log(a);
+
+// //block code
+// if(true){
+// ab=('1000');
+// console.log(ab);
+// }
+// console.log(a);
+
+// //Object (collection of key value pairs)
+// let person = {name:'Godwin'};
+// person.age=26
+// person.home_town= 'Imo'
+// console.log(person);
+
+// //name
+// let name='Godwin Nwachukwu';
+// console.log(typeof name); //type of name
+// console.log(name.length); //length of name
+// console.log(name);
+
+// //number
+// let x=200
+// console.log(typeof x);
+// console.log(x);
+// console.log(x+50);
+
+// let y= '200'
+// console.log(Number(y)); //for converting string to number
+// console.log(typeof y);
+// console.log(y);
+
+// //Boolean (True or false statement)
+// console.log(5>2);
+// console.log(5<2);
+// let a=10
+// let b='10'
+// console.log(a==b); //will check if they have the same value only
+// console.log(a===b); //will check if they have the same value and same data type
+
+// Bigint
+// let x=2n; //The "n" shows it's a big int
+// console.log(typeof x);
+
+// Symbol
+// let sym1=Symbol('Hello'); //Keyword is Symbol with capital "S"
+// console.log(typeof sym1);
+
+
