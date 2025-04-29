d497e418083981fa5888ad87800e8ad952d08811
unknown <nwachukwugodwin7@gmail.com>
2025-04-29 07:17:00 +0100

    first commit

---
+++ Array.js
@@ -0,0 +1,20 @@
+// //Array with array push
+// let arr=[1,2,3];
+// arr.push(4,5);
+// console.log(arr)
+
+
+// //Array (in square brackets)
+// let list = ['apple',2000,'Godwin',12n,32.4]
+// const newElement = list.push('dog'); //push adds another element at the end
+// console.log(list);
+// list.pop(); //pop deletes the last added element
+// console.log(list);
+// list.unshift('boat'); //unshift adds elements at the beginning of the list
+// console.log(list)
+// list.shift(); // Shift removes elements at the beginning of the list
+// console.log(list);
+// list.splice(1,1); //removes an element inbetween(index number, number of items)
+// console.log(list)
+// list.splice(1,0,2000); //adds an element inbetween(index number, number of items, what you want to add)
+// console.log(list);
